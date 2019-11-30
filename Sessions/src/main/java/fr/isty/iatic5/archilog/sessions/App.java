ebab29b73f1aa2e57c6903fbb1a934ad741d5fec
chaimaJalled <38913659+chaimaJalled@users.noreply.github.com>
2019-11-30 12:42:05 +0100

    Connecto to database

--- Sessions/src/main/java/fr/isty/iatic5/archilog/sessions/App.java
+++ Sessions/src/main/java/fr/isty/iatic5/archilog/sessions/App.java
@@ -3,11 +3,16 @@ package fr.isty.iatic5.archilog.sessions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.isty.iatic5.archilog.sessions.config.SqlUtils;
+
 @SpringBootApplication
 public class App {
 
-public static void main(String[] args) {
-SpringApplication.run(App.class, args);
-}
+	public static void main(String[] args) {
+		SqlUtils sqlUtils = new SqlUtils();
+		sqlUtils.connect();
+		SpringApplication.run(App.class, args);
+
+	}
 
 }
